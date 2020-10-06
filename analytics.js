3f8851ef0b5727be7e1b0120dea58c2aff6df763
romainharand <44243451+romainharand@users.noreply.github.com>
2020-10-06 19:01:23 +0200

    First Test

---
+++ analytics.js
@@ -0,0 +1,8 @@
+var url = window.location.pathname;
+url[] urlArray = url.split("/");
+var lastPath = urlArray[urlArray.length-1];
+
+tC.internalvars.product_url = "";
+var urlPath = window.location.pathname;
+var urlArray = urlPath.split( '/' );
+tC.internalvars.product_url = urlArray[urlArray.length-1];
